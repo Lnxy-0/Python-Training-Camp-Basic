1afadc26a2c1aab855b7f1350944b319a24ddfec
Lnxy-0 <5903123079@email.ncu.edu.cn>
2025-04-30 16:46:08 +0800

    完成dict_operations.py练习

--- exercises/dict_operations.py
+++ exercises/dict_operations.py
@@ -20,4 +20,12 @@ def student_dict_operations(students_dict, operation, *args):
     - 根据操作返回不同结果
     """
     # 请在下方编写代码
-    pass 
+    match operation:
+        case "add" | "update":
+            students_dict[args[0]] = args[1]
+            return students_dict
+        case "remove":
+            del students_dict[args[0]]
+            return students_dict
+        case "get":
+            return students_dict[args[0]]
