2414bbdb195bbb5c06bb28f4ed094709d9b96e08
Lnxy-0 <5903123079@email.ncu.edu.cn>
2025-04-30 20:18:03 +0800

    完成while_loop.py练习

--- exercises/while_loop.py
+++ exercises/while_loop.py
@@ -19,4 +19,9 @@ def find_first_even(numbers):
     - 列表中的第一个偶数，如果没有偶数则返回None
     """
     # 请在下方编写代码
-    pass 
+    i = 0
+    while i < len(numbers) and numbers[i] % 2 != 0:
+        i += 1
+    if i < len(numbers):
+        return numbers[i]
+
